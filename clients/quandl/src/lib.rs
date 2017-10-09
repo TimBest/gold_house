d4d4000e5cae31524a81c6602d65cc38caea64d4
Tim <hello@timbest.net>
2017-10-08 17:04:46 -0700

    untagged

--- clients/quandl/src/lib.rs
+++ clients/quandl/src/lib.rs
@@ -11,16 +11,16 @@ pub mod client {
     use serde_json::Error;
     use serde_json;
 
-    // {"dataset":{"id":11304240,"dataset_code":"GOLD","database_code":"LBMA","name":"Gold Price: London Fixing","description":"Gold Price: London Fixings, London Bullion Market Association (LBMA). Fixing levels are set per troy ounce.\nThe London Gold Fixing Companies set the prices for gold that are globally considered as the international standard for pricing of gold.\nThe Gold price in London is set twice a day by five LBMA Market Makers who comprise the London Gold Market Fixing Limited (LGMFL).\nThe process starts with the announcement from the Chairman of the LGMFL to the other members of the LBMA Market Makers, then relayed to the dealing rooms where customers can express their interest as buyers or sellers and also the quantity they wish to trade. The gold fixing price is then set by collating bids and offers until the supply and demand are matched. At this point the price is announced as the 'Fixed' price for gold and all business is conducted on the basis of that price.","refreshed_at":"2017-10-06T16:50:18.145Z","newest_available_date":"2017-10-06","oldest_available_date":"1968-01-02","column_names":["Date","USD (AM)","USD (PM)","GBP (AM)","GBP (PM)","EURO (AM)","EURO (PM)"],"frequency":"daily","type":"Time Series","premium":false,"limit":1,"transform":null,"column_index":null,"start_date":"1968-01-02","end_date":"2017-10-06","data":[["2017-10-06",1268.2,1261.8,970.43,967.93,1083.93,1078.59]],"collapse":null,"order":null,"database_id":139}}
-
     #[derive(Serialize, Deserialize)]
+    #[serde(untagged)]
     pub enum StringFloat {
-        String(String),
-        Float(f64),
+        Date(String),
+        Price(f64),
     }
 
     #[derive(Serialize, Deserialize)]
     pub struct Dataset {
+        pub dataset_code: String,
         pub data: Vec<Vec<StringFloat>>,
     }
 
@@ -42,6 +42,8 @@ pub mod client {
         // TODO: handle http failure cases
         let mut s = String::new();
         res.read_to_string(&mut s).unwrap();
+
+        // let s = stringify!({"dataset":{"id":11304240,"dataset_code":"2017-10-06","database_code":"LBMA","name":"Gold Price: London Fixing","description":"Gold Price: London Fixings, London Bullion Market Association (LBMA). Fixing levels are set per troy ounce.\nThe London Gold Fixing Companies set the prices for gold that are globally considered as the international standard for pricing of gold.\nThe Gold price in London is set twice a day by five LBMA Market Makers who comprise the London Gold Market Fixing Limited (LGMFL).\nThe process starts with the announcement from the Chairman of the LGMFL to the other members of the LBMA Market Makers, then relayed to the dealing rooms where customers can express their interest as buyers or sellers and also the quantity they wish to trade. The gold fixing price is then set by collating bids and offers until the supply and demand are matched. At this point the price is announced as the 'Fixed' price for gold and all business is conducted on the basis of that price.","refreshed_at":"2017-10-06T16:50:18.145Z","newest_available_date":"2017-10-06","oldest_available_date":"1968-01-02","column_names":["Date","USD (AM)","USD (PM)","GBP (AM)","GBP (PM)","EURO (AM)","EURO (PM)"],"frequency":"daily","type":"Time Series","premium":false,"limit":1,"transform":null,"column_index":null,"start_date":"1968-01-02","end_date":"2017-10-06","data":[["2017-10-06",1268.2,1261.8,970.43,967.93,1083.93,1078.59]],"collapse":null,"order":null,"database_id":139}});
 
         return serde_json::from_str(&s).unwrap();
     }
--- jobs/src/main.rs
+++ jobs/src/main.rs
@@ -4,36 +4,48 @@ use std::fs::File;
 use std::path::Path;
 
 extern crate quandl;
+extern crate rustc_serialize;
 
 use quandl::client;
 use quandl::client::StringFloat;
+use rustc_serialize::json;
+
+#[derive(RustcEncodable)]
+struct Commodities {
+    gold: f64,
+}
 
 fn main() {
-    let v = client::get_gold("****".to_string());
+    let v = client::get_gold("***".to_string());
     let usd_per_troy_ounce: f64 = match v.dataset.data[0][1] {
-        StringFloat::String(ref s) => 0 as f64,
-        StringFloat::Float(f) => f,
+        StringFloat::Date(ref s) => 0.0 as f64,
+        StringFloat::Price(f) => f,
     };
     let troy_ounces_of_gold_in_one_cubic_foot = 17554.48;
-    let gold = usd_per_troy_ounce * troy_ounces_of_gold_in_one_cubic_foot;
-    println!("{}", gold);
-    // let path = Path::new("commodities.txt");
-    // let display = path.display();
-    //
-    // // Open a file in write-only mode, returns `io::Result<File>`
-    // let mut file = match File::create(&path) {
-    //     Err(why) => panic!("couldn't create {}: {}",
-    //                        display,
-    //                        why.description()),
-    //     Ok(file) => file,
-    // };
-    //
-    // // Write the `LOREM_IPSUM` string to `file`, returns `io::Result<()>`
-    // match file.write_all(LOREM_IPSUM.as_bytes()) {
-    //     Err(why) => {
-    //         panic!("couldn't write to {}: {}", display,
-    //                                            why.description())
-    //     },
-    //     Ok(_) => println!("successfully wrote to {}", display),
-    // }
+    let gold: f64 = usd_per_troy_ounce * troy_ounces_of_gold_in_one_cubic_foot;
+
+    let commodities = Commodities {
+        gold: gold
+    };
+
+
+    let path = Path::new("commodities.txt");
+    let display = path.display();
+
+    // Open a file in write-only mode, returns `io::Result<File>`
+    let mut file = match File::create(&path) {
+        Err(why) => panic!("couldn't create {}: {}",
+                           display,
+                           why.description()),
+        Ok(file) => file,
+    };
+
+    // Write the `LOREM_IPSUM` string to `file`, returns `io::Result<()>`
+    match file.write_all(json::encode(&commodities).unwrap().as_bytes()) {
+        Err(why) => {
+            panic!("couldn't write to {}: {}", display,
+                                               why.description())
+        },
+        Ok(_) => println!("successfully wrote to {}", display),
+    }
 }
